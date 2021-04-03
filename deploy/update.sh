a4531e2ad542b63c65aa5ef1885d0607ec41ecf2
naresh-khatri <naresh.khatri2345@gmail.com>
2021-04-03 09:40:22 +0530

    ready to deploy on heroku

--- deploy/update.sh
+++ deploy/update.sh
@@ -7,6 +7,6 @@ PROJECT_BASE_PATH='/usr/local/apps/rest_plasmatch'
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-supervisorctl restart profile_api
+supervisorctl restart todos_api
 
 echo "DONE! :)"
